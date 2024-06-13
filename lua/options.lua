c3e6e84fae5b7693ad519d1b69286752d3fa44f0
Pete Richardson <pete@peterichardson.com>
2024-06-13 09:42:36 -0700

    Updated fold settings and integrated old settings from previous .vimrc

--- lua/options.lua
+++ lua/options.lua
@@ -4,6 +4,7 @@ vim.opt.guifont =  {"Hack Nerd Font Mono", ":h24" }
 vim.opt.clipboard = 'unnamedplus'   -- use system clipboard 
 vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
 vim.opt.mouse = 'a'                 -- allow the mouse to be used in Nvim
+vim.opt.backspace = [[indent,eol,start]]
 
 -- Tab
 vim.opt.tabstop = 4                 -- number of visual spaces per TAB
@@ -13,24 +14,32 @@ vim.opt.expandtab = true            -- tabs are spaces, mainly because of python
 
 -- UI config
 vim.opt.number = true               -- show absolute number
--- vim.opt.relativenumber = true       -- add numbers to each line on the left side
 vim.opt.cursorline = true           -- highlight cursor line underneath the cursor horizontally
 vim.opt.splitbelow = true           -- open new vertical split bottom
 vim.opt.splitright = true           -- open new horizontal splits right
 vim.opt.termguicolors = true        -- enabl 24-bit RGB color in the TUI
--- vim.opt.showmode = false         -- we are experienced, wo don't need the "-- INSERT --" mode hint
+vim.opt.showmode = false         -- we are experienced, wo don't need the "-- INSERT --" mode hint
 vim.opt.sidescroll=1
 vim.opt.wrap = false                 -- don't wrap lines
+vim.opt.whichwrap:append("h,l,~,[,],<,>,b,s")
+vim.opt.showmatch = true
+vim.opt.virtualedit:append("all")
+vim.opt.wildmenu = true
+vim.opt.filetype = "on"
+
 -- Searching
 vim.opt.incsearch = true            -- search as characters are entered
--- vim.opt.hlsearch = false            -- do not highlight matches
+vim.opt.hlsearch = true            -- do not highlight matches
 vim.opt.ignorecase = true           -- ignore case in searches by default
 vim.opt.smartcase = true            -- but make it case sensitive if an uppercase is entered
 
 -- Treesitter folding 
 vim.wo.foldmethod = 'expr'
 vim.wo.foldexpr = 'nvim_treesitter#foldexpr()'
+vim.opt.foldenable = false
+vim.opt.foldcolumn ='1'
 
+-- Minimap
 vim.g.minimap_width = 10
 vim.g.minimap_auto_start = 1
 vim.g.minimap_auto_start_win_enter = 1
@@ -38,4 +47,3 @@ vim.g.minimap_window_width_override_for_scaling = 200
 vim.g.minimap_highlight_search = 1
 vim.g.minimap_left = 0
 vim.g.minimap_block_filetypes = {'nvimtree', 'fugitive', 'nerdtree', 'tagbar', 'fzf'}
-vim.opt.foldcolumn ='1'

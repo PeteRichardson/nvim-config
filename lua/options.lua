401aa666b970c1f8a46545b8d701197b41269390
Pete Richardson <pete@peterichardson.com>
2024-06-20 15:58:29 -0700

    added GitSigns and Copilot

--- lua/options.lua
+++ lua/options.lua
@@ -1,26 +1,26 @@
-vim.opt.guifont =  {"Hack Nerd Font Mono", ":h24" }
+vim.opt.guifont = { "Hack Nerd Font Mono", ":h24" }
 
 -- Hint: use `:h <option>` to figure out the meaning if needed
-vim.opt.clipboard = 'unnamedplus'   -- use system clipboard 
-vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
-vim.opt.mouse = 'a'                 -- allow the mouse to be used in Nvim
+vim.opt.clipboard = 'unnamedplus' -- use system clipboard
+vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
+vim.opt.mouse = 'a'               -- allow the mouse to be used in Nvim
 vim.opt.backspace = [[indent,eol,start]]
 
 -- Tab
-vim.opt.tabstop = 4                 -- number of visual spaces per TAB
-vim.opt.softtabstop = 4             -- number of spacesin tab when editing
-vim.opt.shiftwidth = 4              -- insert 4 spaces on a tab
-vim.opt.expandtab = true            -- tabs are spaces, mainly because of python
+vim.opt.tabstop = 4      -- number of visual spaces per TAB
+vim.opt.softtabstop = 4  -- number of spacesin tab when editing
+vim.opt.shiftwidth = 4   -- insert 4 spaces on a tab
+vim.opt.expandtab = true -- tabs are spaces, mainly because of python
 
 -- UI config
-vim.opt.number = true               -- show absolute number
-vim.opt.cursorline = true           -- highlight cursor line underneath the cursor horizontally
-vim.opt.splitbelow = true           -- open new vertical split bottom
-vim.opt.splitright = true           -- open new horizontal splits right
-vim.opt.termguicolors = true        -- enabl 24-bit RGB color in the TUI
-vim.opt.showmode = true             -- show INSERT, NORMAL, etc
-vim.opt.sidescroll=1
-vim.opt.wrap = false                 -- don't wrap lines
+vim.opt.number = true        -- show absolute number
+vim.opt.cursorline = true    -- highlight cursor line underneath the cursor horizontally
+vim.opt.splitbelow = true    -- open new vertical split bottom
+vim.opt.splitright = true    -- open new horizontal splits right
+vim.opt.termguicolors = true -- enabl 24-bit RGB color in the TUI
+vim.opt.showmode = true      -- show INSERT, NORMAL, etc
+vim.opt.sidescroll = 1
+vim.opt.wrap = false         -- don't wrap lines
 vim.opt.whichwrap:append("h,l,~,[,],<,>,b,s")
 vim.opt.showmatch = true
 vim.opt.wildmenu = true
@@ -28,18 +28,20 @@ vim.opt.filetype = "on"
 -- vim.g.mapleader = ","
 
 -- Searching
-vim.opt.incsearch = true            -- search as characters are entered
-vim.opt.hlsearch = true             -- highlight matches
-vim.opt.ignorecase = true           -- ignore case in searches by default
-vim.opt.smartcase = true            -- but make it case sensitive if an uppercase is entered
+vim.opt.incsearch = true  -- search as characters are entered
+vim.opt.hlsearch = true   -- highlight matches
+vim.opt.ignorecase = true -- ignore case in searches by default
+vim.opt.smartcase = true  -- but make it case sensitive if an uppercase is entered
+vim.opt.rtp:append('/opt/homebrew/bin/fzf')
 
--- Treesitter folding 
-vim.opt.fillchars = "fold: "  
-vim.wo.foldmethod = "expr"  
+-- Treesitter folding
+vim.opt.fillchars = "fold: "
+vim.wo.foldmethod = "expr"
 vim.wo.foldexpr = 'nvim_treesitter#foldexpr()'
-vim.opt.foldlevel = 99  
-vim.o.foldtext = [[substitute(getline(v:foldstart),'\\t',repeat('\ ',&tabstop),'g').'...'.trim(getline(v:foldend)) . ' (' . (v:foldend - v:foldstart + 1) . ' lines)']]
-vim.opt.foldcolumn ='2'
+vim.opt.foldlevel = 99
+vim.o.foldtext =
+[[substitute(getline(v:foldstart),'\\t',repeat('\ ',&tabstop),'g').'...'.trim(getline(v:foldend)) . ' (' . (v:foldend - v:foldstart + 1) . ' lines)']]
+vim.opt.foldcolumn = '2'
 
 -- Minimap
 vim.g.minimap_width = 10
@@ -48,4 +50,4 @@ vim.g.minimap_auto_start_win_enter = 1
 vim.g.minimap_window_width_override_for_scaling = 200
 vim.g.minimap_highlight_search = 1
 vim.g.minimap_left = 0
-vim.g.minimap_block_filetypes = {'nvimtree', 'fugitive', 'nerdtree', 'tagbar', 'fzf'}
+vim.g.minimap_block_filetypes = { 'nvimtree', 'fugitive', 'nerdtree', 'tagbar', 'fzf' }
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,73 +1,140 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  --------------------------------------
-  'tpope/vim-sensible',
-  
-  --------------------------------------
-  -- Misc LSP stuff
-  "williamboman/mason.nvim",
-  "williamboman/mason-lspconfig.nvim",
-  "neovim/nvim-lspconfig",
+    --------------------------------------
+    'tpope/vim-sensible',
 
-  --------------------------------------
-  -- moonfly colorscheme
-  "bluz71/vim-moonfly-colors",
+    --------------------------------------
+    -- Misc LSP stuff
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
 
-  --------------------------------------
-  --  Aerial - for listing/navigating language symbols: methods, enums, etc
-  {
-    'stevearc/aerial.nvim',
-    opts = {},
-    -- Optional dependencies
-    dependencies = {
-       "nvim-treesitter/nvim-treesitter",
-       "nvim-tree/nvim-web-devicons"
+    --------------------------------------
+    -- moonfly colorscheme
+    "bluz71/vim-moonfly-colors",
+
+    --------------------------------------
+    --  Aerial - for listing/navigating language symbols: methods, enums, etc
+    {
+        'stevearc/aerial.nvim',
+        opts = {},
+        -- Optional dependencies
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            "nvim-tree/nvim-web-devicons"
+        },
+        keys = {
+            vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr }),
+            vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr }),
+            vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle<CR>"),
+        },
+        lazy = false,
     },
-    keys = {
-        vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr }),
-        vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr }),
-        vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle<CR>"),
+
+    --------------------------------------
+    --
+    {
+        "zbirenbaum/copilot.lua",
+        cmd = "Copilot",
+        event = "InsertEnter",
+        config = function()
+            require("copilot").setup({})
+        end,
     },
-    lazy = false,
-  },
+    --------------------------------------
+    --
+    {
+        'lewis6991/gitsigns.nvim',
+        lazy = false,
+    },
 
-  --------------------------------------
-  -- nvim-tree for navigating the file system
-  'nvim-tree/nvim-web-devicons',
-  {
-    "nvim-tree/nvim-tree.lua",
-    sort = { sorter = "case_sensitive" },
-    view = { width = 15 },
-    renderer = { group_empty = true },
-    filters = { dotfiles = true },
-    -- keys = {
-    --     {"n", "<leader>a", ":NvimTreeToggle<CR>", { silent = true }}
-    -- },
-    lazy = false,
-  },
+    --------------------------------------
+    -- nvim-tree for navigating the file system
+    'nvim-tree/nvim-web-devicons',
+    {
+        "nvim-tree/nvim-tree.lua",
+        sort = { sorter = "case_sensitive" },
+        view = { width = 15 },
+        renderer = { group_empty = true },
+        filters = { dotfiles = true },
+        -- keys = {
+        --     {"n", "<leader>a", ":NvimTreeToggle<CR>", { silent = true }}
+        -- },
+        lazy = false,
+    },
 
-  --------------------------------------
-  -- rustaceanvim - better fork of rust-tools
-  {
-    'mrcjkb/rustaceanvim',
-    version = '^4', -- Recommended
-    lazy = false, -- This plugin is already lazy
-  },
+    --------------------------------------
+    -- rustaceanvim - better fork of rust-tools
+    {
+        'mrcjkb/rustaceanvim',
+        version = '^4', -- Recommended
+        lazy = false,   -- This plugin is already lazy
+    },
 
-  -- 'wfxr/minimap.vim',
+    --------------------------------------
+    -- fzf plugin to improve file searching
+    -- 'junegu:web/fzf',
+    -- 'junegunn/fzf.vim',
+
+    -- 'wfxr/minimap.vim',
 })
 
-require("nvim-tree").setup {}   
+require("nvim-tree").setup {}
+require('gitsigns').setup {
+    on_attach = function(bufnr)
+        local gitsigns = require('gitsigns')
 
+        local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Navigation
+        map('n', ']c', function()
+            if vim.wo.diff then
+                vim.cmd.normal({ ']c', bang = true })
+            else
+                gitsigns.nav_hunk('next')
+            end
+        end)
+
+        map('n', '[c', function()
+            if vim.wo.diff then
+                vim.cmd.normal({ '[c', bang = true })
+            else
+                gitsigns.nav_hunk('prev')
+            end
+        end)
+
+        -- Actions
+        map('n', '<leader>hs', gitsigns.stage_hunk)
+        map('n', '<leader>hr', gitsigns.reset_hunk)
+        map('v', '<leader>hs', function() gitsigns.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
+        map('v', '<leader>hr', function() gitsigns.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
+        map('n', '<leader>hS', gitsigns.stage_buffer)
+        map('n', '<leader>hu', gitsigns.undo_stage_hunk)
+        map('n', '<leader>hR', gitsigns.reset_buffer)
+        map('n', '<leader>hp', gitsigns.preview_hunk)
+        map('n', '<leader>hb', function() gitsigns.blame_line { full = true } end)
+        map('n', '<leader>tb', gitsigns.toggle_current_line_blame)
+        map('n', '<leader>hd', gitsigns.diffthis)
+        map('n', '<leader>hD', function() gitsigns.diffthis('~') end)
+        map('n', '<leader>td', gitsigns.toggle_deleted)
+
+        -- Text object
+        map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+    end
+}
